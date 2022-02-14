4ebcd32c6302a6b87e72e5bc7f12f43549c55d13
jesse <jesse@glue.be>
2022-02-14 11:48:25 +0100

    rename module

--- registration.php
+++ registration.php
@@ -2,6 +2,6 @@
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Glue_ExtendedSearch',
+    'GlueTeam_ExtendedSearch',
     __DIR__
 );
